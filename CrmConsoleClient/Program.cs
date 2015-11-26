03f4388ab2cee82ecb84a4077053c1e394cb452b
meanprogrammer <mean.programmer.d@gmail.com>
2015-11-26 17:43:37 +0800

    initial

---
+++ CrmConsoleClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BPService.BaselineProjectionService client = new BPService.BaselineProjectionService();
+            var bps = client.GetAllBaselineProjection();
+            
+            /*
+            StringBuilder builder = new StringBuilder();
+            foreach (var item in bps)
+            {
+
+                builder.AppendLine(string.Format("{0}-{1}", item.Id, item.Name));
+            }
+
+            File.WriteAllText(@"C:\Users\vd2\dumpfile.txt", builder.ToString());
+            */
+
+            //533c17a0-7666-e511-80f8-3863bb2e7cd8
+
+            BPService.CustomBaselineProjection filter = new BPService.CustomBaselineProjection();
+            filter.Id = "533c17a0-7666-e511-80f8-3863bb2e7cd8";
+
+            client.UpdateBaselineProjection(filter);
+        }
+    }
+}
